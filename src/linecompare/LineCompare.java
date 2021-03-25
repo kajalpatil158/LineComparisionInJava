1210e4bc6fdf42849aea6357bbbc37df75f2e1ee
Kajal <kajalpatil158@gmail.com>
2021-03-25 10:26:06 +0530

    [Kajal Patil]Added Squrt Of Line

--- src/linecompare/LineCompare.java
+++ src/linecompare/LineCompare.java
@@ -1,11 +1,25 @@
 package linecompare;
 
+import java.util.Scanner;
+
 public class LineCompare {
 
 	public static void main(String[] args) {
 		// Message
 		System.out.println("Well Come To Line Comparision Computation Program");
-
+		// Computation
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter x1 ");
+		int x1 = sc.nextInt();
+		System.out.print("Enter x2 ");
+		int x2 = sc.nextInt();
+		System.out.print("Enter y1 ");
+		int y1 = sc.nextInt();
+		System.out.print("Enter y2 ");
+		int y2 = sc.nextInt();
+		System.out.print("Enter A1 ");
+		int length = ((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		System.out.println("Lengh of line is:" + Math.sqrt(length));
 	}
 
 }
