3a6d29185e86cf46bbf92c9d5358fefe5343fa20
Kajal <kajalpatil158@gmail.com>
2021-03-25 10:21:53 +0530

    [Kajal Patil]Added: Well Come Message

---
+++ src/linecompare/LineCompare.java
@@ -0,0 +1,11 @@
+package linecompare;
+
+public class LineCompare {
+
+	public static void main(String[] args) {
+		// Message
+		System.out.println("Well Come To Line Comparision Computation Program");
+
+	}
+
+}
